d05e69096d590d99be737a0f60136aabc84d48f4
hika <hika00@gmail.com>
2015-05-04 19:25:30 +0900

    생성자체인연쇄문제 해결 #19

--- src/Mogl.js
+++ src/Mogl.js
@@ -1,46 +1,73 @@
 var MoGL = (function(){
-	var isFactory, MoGL, fn;
+	var isFactory, isSuperChain, uuid, counter, totalCount, MoGL, fn;
 	//팩토리 함수용 식별상수
-	isFactory = {},
+	isFactory = {factory:1},
+	//생성자체인용 상수
+	isSuperChain = {superChain:1},
+	//인스턴스 카운트 시스템
+	uuid = 0,//모든 인스턴스는 고유한 uuid를 갖게 됨.
+	totalCount = 0, //생성된 인스턴스의 갯수를 관리함
+	counter = {}, //클래스별로 관리
 	
-	MoGL = function(){},
-	//method
+	//생성자
+	MoGL = function(){
+		this.uuid = uuid++;
+		counter[this.constructor.uuid]++;
+		totalCount++;
+	},
 	fn = MoGL.prototype,
-
-
-	//static method   
-
+	//파괴자
+	fn.destroy = function destroy(){
+		var key;
+		for( key in this ) if( this.hasOwnProperty(key) ) this[key] = null;
+		counter[this.constructor.uuid]--;
+		totalCount--;
+	};
 	//parent클래스를 상속하는 자식클래스를 만들어냄.
 	MoGL.ext = function ext( child, parent ){
 		var cls, oldProto, newProto, key;
 		//생성자클래스
 		cls = function(){
 			var arg;
-			if( this instanceof cls ){
+			if( arguments[0] === isSuperChain ){
+				parent.apply( this, arguments[1] );
+				child.apply( this, arguments[1] );
+			}else if( this instanceof cls ){
 				if( arguments[0] === isFactory ){
 					arg = arguments[1];
 				}else{
 					arg = arguments;
 				}
-				parent.apply( this, arg );
+				parent.call( this, isSuperChain, arg );
 				child.apply( this, arg );
 				return this;
 			}else{
 				return new cls( isFactory, arguments );
 			}
 		};
-
-		//자식 클래스의 프로토타입을 옮기고 부모와 체이닝함.
-		newProto = new parent;
+		cls._uuid = uuid++;
+		cls._parent = parent;
+		if( !( cls.uuid in counter ) ) counter[cls.uuid] = 0;
+		
+		//부모와 자식 클래스의 프로토타입을 옮김.
+		newProto = cls.prototype;
+		oldProto = parent.prototype;
+		for( key in oldProto ) if( oldProto.hasOwnProperty(key) ) newProto[key] = oldProto[key];
 		oldProto = child.prototype;
 		for( key in oldProto ) if( oldProto.hasOwnProperty(key) ) newProto[key] = oldProto[key];
-		cls.prototype = newProto;
 		return cls;
 	};
 	//표준 error처리
-	MoGL.error = function( cls, method, id ){
+	MoGL.error = function error( cls, method, id ){
 		throw new Error( cls + '.' + method + ':' + id );
 	};
-
+	//인스턴스의 갯수를 알아냄
+	MoGL.count = function count( cls ){
+		if( typeof cls == 'function' ){
+			return counter[cls.uuid];
+		}else{
+			return totalCount;
+		}
+	};
 	return MoGL;
 })();
