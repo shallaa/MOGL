3e2d424c70f9bb2135caf8c8bae3cb43685eb88c
Redcamel <webseon@gmail.com>
2015-05-05 17:20:04 +0900

    Scene Dev001

--- src/Scene.js
+++ src/Scene.js
@@ -1,10 +1,74 @@
 /**
  * Created by redcamel on 2015-05-05.
  */
+'use strict'
 var Scene = (function () {
     var Scene, fn;
     Scene = function Scene() {
-    }
-    fn = Scene.prototype
+        this.children = {},
+            this.shaderList = {v: {}, f: {}},
+            this.textureList = {}, this.materialList = {},
+            this.geometryList = {}
+    },
+        fn = Scene.prototype,
+        fn.addChild = function addChild($id, $target) {
+            var t = 1
+            this.children[$id] ? (MoGL.error('Scene', 'addChild', 0), t = 0) : 0
+            $target instanceof Mesh ? 0 : (MoGL.error('Scene', 'addChild', 1), t = 0)
+            // TODO Mesh안의 Geometry에 지정된 vertex shader의 id가 존재하지 않음.
+            // TODO Mesh안의 Material에 지정된 fragment shader의 id가 존재하지 않음.
+            t ? this.children[$id] = $target : 0
+            console.log(this)
+            return this
+        },
+        fn.addGeometry = function ($id, $target) {
+            var t = 1
+            this.geometryList[$id] ? (MoGL.error('Scene', 'addGeometry', 0), t = 0) : 0
+            $target instanceof Geometry ? 0 : (MoGL.error('Scene', 'addGeometry', 1), t = 0)
+            //TODO 'Scene.addGeometry:2' - Geometry에 선언된 vertex shader의 id가 없을 때.
+            t ? this.geometryList[$id] = $target : 0
+            return this
+        },
+        fn.addMaterial = function ($id, $target) {
+            var t = 1
+            this.materialList[$id] ? (MoGL.error('Scene', 'addMaterial', 0), t = 0) : 0
+            $target instanceof Material ? 0 : (MoGL.error('Scene', 'addMaterial', 1), t = 0)
+            //TODO 'Scene.addMaterial:2' - Material에 선언된 fragment shader의 id가 없을 때.
+            t ? this.materialList[$id] = $target : 0
+            //TODO 텍스쳐 제너레이터
+            return this
+        },
+        fn.addTexture = function addTexture($id, $target/*,resizeType*/) {
+            var t = 1
+            this.textureList[$id] ? (MoGL.error('Scene', 'addTexture', 0), t = 0) : 0
+            //TODO Param에 명시된 형식이 아닌 image를 등록하려할 때.
+            t ? this.textureList[$id] = $target : 0
+            //TODO 텍스쳐 제너레이터
+            return this
+        },
+        fn.addFragmentShader = function () {},//TODO
+        fn.addVertextShader = function () {},//TODO
+        ///////////////////////////////////////////////////////////////////////////
+        // Get
+        fn.getChild = function getChild($id) {
+            var t = this.children[$id];
+            return t ? t : null
+        },
+        fn.getGeometry = function getGeometry($id) {
+            var t = this.geometryList[$id];
+            return t ? t : null
+        },
+        fn.getMaterial = function getMaterial($id) {return this.materialList[$id]},
+        fn.getTexture = function getTexture($id) {return this.textureList[$id]},
+        fn.getFragmentShader = function () {},//TODO
+        fn.getVertextShader = function () {},//TODO
+        ///////////////////////////////////////////////////////////////////////////
+        // Remove
+        fn.removeChild = function removeChild($id) { return this.children[$id] ? (delete this.children[$id], true) : false},
+        fn.removeGeometry = function removeGeometry($id) { return this.geometryList[$id] ? (delete this.geometryList[$id], true) : false},
+        fn.removeMaterial = function removeMaterial($id) { return this.materialList[$id] ? (delete this.materialList[$id], true) : false},
+        fn.removeTexture = function removeTexture($id) { return this.textureList[$id] ? (delete this.textureList[$id], true) : false},
+        fn.removeFragmentShader = function () {},//TODO
+        fn.removeVertextShader = function () {}//TODO
     return MoGL.ext(Scene, MoGL);
 })();
--- src/Texture.js
+++ src/Texture.js
@@ -1,5 +1,11 @@
 /**
  * Created by redcamel on 2015-05-05.
+ * description
+ 텍스쳐로 사용될 이미지를 등록함.
+ 로딩이 완료되지 않은 이미지는 등록되지 않고 무시됨.
+ 이미지의 크기가 2의 n승으로 떨어지지 않는 경우 자동으로 리사이즈함.
+ 내부적으로는 어떠한 소스가 와도 image엘리먼트를 만들어 base64형식의 dateURL을 src로 지정한 상태의 객체로 보관함.
+ 내부처리가 있으므로 field로 제공되지 않음.
  */
 var Texture = (function () {
     var Texture, fn;
