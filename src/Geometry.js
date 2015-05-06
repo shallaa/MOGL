2414d7411ea7690f4d6ffa0de52cf1d6d5ef47eb
Redcamel <webseon@gmail.com>
2015-05-06 15:25:02 +0900

    Geometry Dev002

--- src/Geometry.js
+++ src/Geometry.js
@@ -10,10 +10,8 @@ var Geometry = (function () {
         var t = arguments[2] ? arguments[2].length : 3
         this._vertexCount = $vArray.length / t
         this._triCount = $iArray.length / 3
-        this._shaderID = null
-
+        this._shaderIDList = {}
         //TODO $vArray 판별 을 어케할지 고민
-
         // get Volume
         var minX = 0, minY = 0, minZ = 0
         var maxX = 0, maxY = 0, maxZ = 0
@@ -41,18 +39,13 @@ var Geometry = (function () {
         fn.NORMAL_Z = 'normalZ',
         fn.U = 'u',
         fn.V = 'v',
-        fn.addVertxShader = function addVertxShader($id) {
-            // TODO addVertxShader 구현
-            //Mesh를 통해 최종적으로 포함될 Scene에 등록된 shader를 사용함.
-            //Scene에 직접 등록되는 경우는 id를 addGeometry 시점에 평가함.
-            //Mesh에서 직접 생성하여 삽입하는 경우는 addChild시점에 평가함.
-            //이미 직간접적으로 Scene에 포함된 경우는 메서드호출시점에 평가함.
-
-            return this._shaderID = $id, this
+        fn.addVertexShader = function addVertexShader($id) {
+            this._shaderIDList[$id] = $id
+            return this
         },
         fn.getVertexCount = function getVertexCount() {return this._vertexCount},
         fn.getTriangleCount = function getTriangleCount() {return this._triCount},
         fn.getVolume = function getVolume() {return this._volume},
-        fn.removeVertexShader = function removeVertexShader() {return this._shaderID = null, this}
+        fn.removeVertexShader = function removeVertexShader($id) {return delete this._shaderIDList[$id], this}
     return MoGL.ext(Geometry, MoGL);
 })();
