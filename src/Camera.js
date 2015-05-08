38be54c61f6e91b9cf8c26296bbcc9a540454698
Redcamel <webseon@gmail.com>
2015-05-08 17:39:52 +0900

    렌더 기본구조...및 먼가또 인코딩타입이 꼬여서 업뎃

--- src/Camera.js
+++ src/Camera.js
@@ -11,7 +11,7 @@ var Camera = (function () {
     fn = Camera.prototype,
     fn.render = function render(scene){
         // 먼가 차일드를 루프돌면 되것군..
-        console.log('카메라렌더 대상은 : ',scene)
+        console.log('카메라렌더',arguments[1],arguments[2], '실제 Scene : ',scene)
     }
     return MoGL.ext(Camera, Mesh);
 })();
--- src/World.js
+++ src/World.js
@@ -15,7 +15,7 @@ var World = (function () {
         for(var i = 0,len=this._renderList.length; i<len; i++){
             //console.log(this._renderList[i],'렌더')
             // 여기서 할일은 렌더리스트의 아이템에있는 카메라에 씬을 던져서 실제 렝더링을 시켜야함..
-            this._renderList[i].camera.render(this._renderList[i].scene)
+            this._renderList[i].camera.render(this._renderList[i].scene,this._renderList[i].sceneID,this._renderList[i].cameraID)
         }
     },
     fn.addRender = function addRender(sceneID, cameraID, index) { MoGL.isAlive(this);
