0e048cc7c78cdab9ca9b7c1940005090aa02f8d3
Redcamel <webseon@gmail.com>
2015-05-13 12:32:18 +0900

    World에 렌더러를 옮김

--- src/Camera.js
+++ src/Camera.js
@@ -8,7 +8,6 @@ var Camera = (function () {
     Camera = function Camera() {
         this._geometry = new Geometry([], [])
         this._material = new Material()
-        this._isCamera = 1
         this._r = 0,
         this._g = 0,
         this._b = 0,
@@ -23,54 +22,25 @@ var Camera = (function () {
             if (t0.charAt(0) == '#') {
                 if (t1 = hex.exec(t0)) {
                     this._r = parseInt(t1[1], 16) / 255,
-                        this._g = parseInt(t1[2], 16) / 255,
-                        this._b = parseInt(t1[3], 16) / 255
+                    this._g = parseInt(t1[2], 16) / 255,
+                    this._b = parseInt(t1[3], 16) / 255
 
                 } else {
                     t1 = hex_s.exec(t0),
-                        this._r = parseInt(t1[1] + t1[1], 16) / 255,
-                        this._g = parseInt(t1[2] + t1[2], 16) / 255,
-                        this._b = parseInt(t1[3] + t1[3], 16) / 255
+                    this._r = parseInt(t1[1] + t1[1], 16) / 255,
+                    this._g = parseInt(t1[2] + t1[2], 16) / 255,
+                    this._b = parseInt(t1[3] + t1[3], 16) / 255
                 }
                 this._a = ta ? ta > 1 ? 1 : ta : 1
             }
         } else {
             this._r = arguments[0],
-                this._g = arguments[1],
-                this._b = arguments[2],
-                this._a = arguments[3] ? arguments[3] : 1
+            this._g = arguments[1],
+            this._b = arguments[2],
+            this._a = arguments[3] ? arguments[3] : 1
         }
         return this
     }
-    fn.render = function render(scene) {
-        MoGL.isAlive(this);
-        // 먼가 차일드를 루프돌면 되것군..
-        //console.log('카메라렌더',arguments[1],arguments[2], '실제 Scene : ',scene)
 
-        var gl = scene._gl
-        var children = scene._children
-        gl.clearColor(this._r, this._g, this._b, this._a)
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-        var tItem, tMaterial, tProgram, tVBO, tIBO
-        var len = 0
-        for (var k in children) {
-            tItem = children[k]
-            tVBO = scene._glVBOs[tItem._geometry._name]
-            tIBO = scene._glIBOs[tItem._geometry._name]
-            tMaterial = tItem._material
-            tProgram = scene._glPROGRAMs['base'] // TODO 이놈은 어디서 결정하지?
-            gl.useProgram(tProgram)
-            gl.bindBuffer(gl.ARRAY_BUFFER, tVBO),
-                gl.vertexAttribPointer(tProgram.aVertexPosition, tVBO.stride, gl.FLOAT, false, 0, 0),
-                gl.uniform3fv(tProgram.uRotate, [tItem.rotateX, tItem.rotateY, tItem.rotateZ]),
-                gl.uniform3fv(tProgram.uPosition, [tItem.x, tItem.y, tItem.z]),
-                gl.uniform3fv(tProgram.uScale, [tItem.scaleX, tItem.scaleY, tItem.scaleZ]),
-                gl.uniform3fv(tProgram.uColor, [tMaterial._r, tMaterial._g, tMaterial._b]),
-                gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tIBO),
-                gl.drawElements(gl.TRIANGLES, tIBO.numItem, gl.UNSIGNED_SHORT, 0)
-            len++
-        }
-        //console.log(len)
-    }
     return MoGL.ext(Camera, Mesh);
 })();
--- src/World.js
+++ src/World.js
@@ -17,12 +17,54 @@ var World = (function () {
     fn._renderList = [],
     fn._sceneList = {},
     fn._render = function _render() { MoGL.isAlive(this);
-        var i, len, tList = this._renderList
+        var i, k, len, tList = this._renderList
+        var scene,camera,gl,children;
+        var tItem, tMaterial, tProgram, tVBO, tUVBO, tIBO;
         for (i = 0, len = tList.length; i < len; i++) {
             //console.log(tList[i],'렌더')
-            // 여기서 할일은 렌더리스트의 아이템에있는 카메라에 씬을 던져서 실제 렝더링을 시켜야함..
             if (tList[i].scene._update) tList[i].scene.update()
-            tList[i].camera.render(tList[i].scene, tList[i].sceneID, tList[i].cameraID)
+            //console.log('카메라렌더',tList[i].sceneID,tList[i].cameraID, '실제 Scene : ',tList[i].scene)
+            scene = tList[i].scene,
+            camera = scene.getChild(tList[i].cameraID),
+            gl = scene._gl,
+            children = scene._children,
+            gl.clearColor(camera._r, camera._g, camera._b, camera._a),
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT),
+            tItem = tMaterial = tProgram = tVBO = tIBO = null
+            for (k in children) {
+                tItem = children[k],
+                tVBO = scene._glVBOs[tItem._geometry._name],
+                tUVBO = scene._glUVBOs[tItem._geometry._name],
+                tIBO = scene._glIBOs[tItem._geometry._name],
+                tMaterial = tItem._material,
+                tProgram = tMaterial._textures.__indexList.length>0 ?scene._glPROGRAMs['bitmap'] :scene._glPROGRAMs['base'], // TODO 이놈은 어디서 결정하지?
+                gl.useProgram(tProgram)
+                if(tProgram==scene._glPROGRAMs['base']){
+                    gl.bindBuffer(gl.ARRAY_BUFFER, tVBO),
+                    gl.vertexAttribPointer(tProgram.aVertexPosition, tVBO.stride, gl.FLOAT, false, 0, 0),
+                    gl.uniform3fv(tProgram.uRotate, [tItem.rotateX, tItem.rotateY, tItem.rotateZ]),
+                    gl.uniform3fv(tProgram.uPosition, [tItem.x, tItem.y, tItem.z]),
+                    gl.uniform3fv(tProgram.uScale, [tItem.scaleX, tItem.scaleY, tItem.scaleZ]),
+                    gl.uniform3fv(tProgram.uColor, [tMaterial._r, tMaterial._g, tMaterial._b]),
+                    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tIBO),
+                    gl.drawElements(gl.TRIANGLES, tIBO.numItem, gl.UNSIGNED_SHORT, 0)
+                }else  if(tProgram==scene._glPROGRAMs['bitmap']){
+                    gl.bindBuffer(gl.ARRAY_BUFFER, tVBO),
+                    gl.vertexAttribPointer(tProgram.aVertexPosition, tVBO.stride, gl.FLOAT, false, 0, 0),
+                    gl.bindBuffer(gl.ARRAY_BUFFER, tUVBO),
+                    gl.vertexAttribPointer(tProgram.aUV, tUVBO.stride, gl.FLOAT, false, 0, 0),
+                    gl.uniform3fv(tProgram.uRotate, [tItem.rotateX, tItem.rotateY, tItem.rotateZ]),
+                    gl.uniform3fv(tProgram.uPosition, [tItem.x, tItem.y, tItem.z]),
+                    gl.uniform3fv(tProgram.uScale, [tItem.scaleX, tItem.scaleY, tItem.scaleZ]),
+                    gl.activeTexture(gl.TEXTURE0);
+                    var texture = scene._glTEXTUREs[tMaterial._textures.__indexList[0].id]
+                    gl.bindTexture(gl.TEXTURE_2D, texture);
+                    gl.uniform1i(tProgram.uSampler, 0);
+                    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, tIBO),
+                    gl.drawElements(gl.TRIANGLES, tIBO.numItem, gl.UNSIGNED_SHORT, 0)
+
+                }
+            }
         }
     },
     fn.addRender = function addRender(sceneID, cameraID, index) { MoGL.isAlive(this);
