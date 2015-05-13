15778e91dccb36fc7fd78b68dac47040a6a37b51
Redcamel <webseon@gmail.com>
2015-05-13 13:33:10 +0900

    카메라 스켈레톤 작성

--- src/Camera.js
+++ src/Camera.js
@@ -3,7 +3,7 @@
  * description
  */
 var Camera = (function () {
-    var Camera, fn;
+    var Camera, fn,a4=[];
     var hex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i, hex_s = /^#?([a-f\d]{1})([a-f\d]{1})([a-f\d]{1})$/i;
     Camera = function Camera() {
         this._geometry = new Geometry([], [])
@@ -14,8 +14,26 @@ var Camera = (function () {
         this._a = 1
     }
     fn = Camera.prototype
-    fn.setBackgroundColor = function setBackgroundColor() {
-        MoGL.isAlive(this);
+    fn.getBackgroundColor = function getBackgroundColor(){MoGL.isAlive(this);
+        return a4[0] = this._r, a4[1] = this._g, a4[2] = this._b, a4[3] = this._a, a4
+    },
+    fn.getClipPlane = function getClipPlane(){MoGL.isAlive(this);
+    },
+    fn.getFilters = function getFilters(){MoGL.isAlive(this);
+    },
+    fn.getFog = function getFog(){MoGL.isAlive(this);
+    },
+    fn.getFOV = function getFOV(){MoGL.isAlive(this);
+    },
+    fn.getProjectionMatrix = function getProjectionMatrix(){MoGL.isAlive(this);
+    },
+    fn.getRenderArea = function getRenderArea(){MoGL.isAlive(this);
+    },
+    fn.getStereo = function getStereo(){MoGL.isAlive(this);
+    },
+    fn.getVisible = function getVisible(){MoGL.isAlive(this);
+    },
+    fn.setBackgroundColor = function setBackgroundColor() {MoGL.isAlive(this);
         var t0 = arguments[0], t1, ta
         if (arguments.length == 1) {
             if (t0.length > 7) ta = +t0.substr(7), t0 = t0.substr(0, 7)
@@ -40,7 +58,39 @@ var Camera = (function () {
             this._a = arguments[3] ? arguments[3] : 1
         }
         return this
+    },
+    fn.setClipPlane = function setClipPlane(){MoGL.isAlive(this);
+        return this
+    },
+    fn.setFilter = function setFilter(){MoGL.isAlive(this);
+        return this
+    },
+    fn.setFog = function setFog(){MoGL.isAlive(this);
+        return this
+    },
+    fn.setFOV = function setFOV(){MoGL.isAlive(this);
+        return this
+    },
+    fn.setOthogonal = function setOthogonal(){MoGL.isAlive(this);
+        return this
+    },
+    fn.setPerspective = function setPerspective(){MoGL.isAlive(this);
+        return this
+    },
+    fn.setProjectionMatrix = function setProjectionMatrix(){MoGL.isAlive(this);
+        return this
+    },
+    fn.setRenderArea = function setRenderArea(){MoGL.isAlive(this);
+        return this
+    },
+    fn.setStereo = function setStereo(){MoGL.isAlive(this);
+        return this
+    },
+    fn.setVisible = function setVisible(){MoGL.isAlive(this);
+        return this
+    },
+    fn.removeFilter = function removeFilter(){MoGL.isAlive(this);
+        return this
     }
-
     return MoGL.ext(Camera, Mesh);
 })();
