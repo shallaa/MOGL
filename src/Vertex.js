e12e29266cb2ce016f1ecac745da65e342050e4a
shallaa <shallaa@shallaazm.com>
2015-05-14 16:27:44 +0900

    master 브랜치 src 제거.

--- src/Vertex.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by redcamel on 2015-05-07.
- */
-var Vertex = {
-    x: 'x', y: 'y', z: 'z',
-    r: 'r', g: 'g', b: 'b', a: 'a',
-    normalX: 'nx', normalY: 'ny', normalZ: 'nz',
-    u: 'u', v: 'v'
-}
